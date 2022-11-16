8761bfb0dbbe3494a9e9da83866cc86503022120
Febryan Ramadhan <febryanramadhan@gmail.com>
2022-11-16 11:37:06 +0700

    progress script, no validation used nat.txt

--- add-rules-iptables.sh
+++ add-rules-iptables.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
-LIST_IP=`cat $1 | grep IP1 | awk '{print $2}'`
-LIST_NAT_IP=`cat $2 | grep IP_TARGET | cut -d "=" -f 2`
-for ip in $LIST_NAT_IP;
-do
+LIST_NAT=$(cat $1 | grep IP_NAT | awk -F '[][]' '{print $2}')
+for IP_NAT in $LIST_NAT; do
+    PORT_DESTINATION=$(cat $1 | awk "/\<$IP_NAT\>/" RS= $1 | grep PORT_DESTINATION | cut -d "=" -f 2)
+    IP_TARGET=$(cat $1 | awk "/\<$IP_NAT\>/" RS= $1 | grep IP_TARGET | cut -d "=" -f 2)
+    PORT_TARGET=$(cat $1 | awk "/\<$IP_NAT\>/" RS= $1 | grep PORT_TARGET | cut -d "=" -f 2)
 
-IP_TARGET=`cat $2 | grep IP_TARGET | cut -d "=" -f 2`
-if [[ "$LIST_IP" == *"$NAT_IP"* ]];then
-    echo "IP EXISTS, adding rules ..."
-echo $IP_TARGET
-echo $IP_LIST
+    sudo iptables -t nat -A PREROUTING -p tcp --dport $PORT_DESTINATION -j DNAT --to-destination $IP_TARGET:$PORT_TARGET
 
-IP_TARGET2=$IP_TARGET
-#PROTOCOL=`cat $2 | grep PROTOCOL | cut -d "=" -f 2`
-PORT_DESTINATION=`cat $2 | grep PORT_DESTINATION | cut -d "=" -f 2`
-PORT_TARGET=`cat $2 | grep PORT_TARGET | cut -d "=" -f 2`
+    # echo $PORT_DESTINATION
+    echo $IP_TARGET
+    # echo $PORT_TARGET
 
-sudo iptables -t nat -A PREROUTING -p tcp --dport $PORT_DESTINATION -j DNAT --to-destination $IP_TARGET:$PORT_TARGET
-
-LIST=`sudo iptables -t nat -v -L PREROUTING -n --line-number | grep $ip`
-echo "Done added $LIST"
-else
-    echo "IP NOT EXISTS!"
-fi
+    LIST=$(sudo iptables -t nat -v -L PREROUTING -n --line-number | grep $IP_TARGET)
+    echo "Done added $LIST"
 done
